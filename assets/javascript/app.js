de7c8ab701dcf464ef64ee578ee176e873df294c
Min Ryu <myyms@Mins-MacBook-Pro.local>
2017-11-07 23:23:39 -0800

    firebase info pulling complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+  var config = {
+	    apiKey: "AIzaSyDbjKOtSlUj-6UpMuZZW0XC_0GF_e-WN4w",
+	    authDomain: "in-class-demo-min.firebaseapp.com",
+	    databaseURL: "https://in-class-demo-min.firebaseio.com",
+	    projectId: "in-class-demo-min",
+	    storageBucket: "in-class-demo-min.appspot.com",
+	    messagingSenderId: "594198822166"
+    };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    $("#submit-btn").on("click", function(){
+
+    	event.preventDefault();
+
+    	console.log($("#input-train").val());
+    	console.log($("#input-destination").val());
+    	console.log($("#input-time").val());
+    	console.log($("#input-frequency").val());
+
+
+    	database.ref("/train").push({
+
+    		train_name: $("#input-train").val(),
+    		destination: $("#input-destination").val(),
+    		time: $("#input-time").val(),
+    		frequency: $("#input-frequency").val()
+
+    	});
+
+    });
+
+    database.ref("/train").on("child_added",function(snapshot){
+
+    	var newrow = $("<tr>");
+    	var tr1 = $("<td>")
+    	var tr2 = $("<td>")
+    	var tr3 = $("<td>")
+    	var tr4 = $("<td>")
+    	var tr5 = $("<td>")
+
+    	tr1.append(snapshot.val().train_name);
+    	tr2.append(snapshot.val().destination);
+    	tr3.append(snapshot.val().frequency);
+    	tr4.append("blank");
+    	tr5.append("blank");
+
+    	newrow.append(tr1, tr2, tr3, tr4, tr5);
+
+    	$("#train-schedule").append(newrow);
+
+    });
+
+    console.log(moment().format("HH:mm"));
+
+    function convertTime (time){
+
+    	var time = time.split(":")
+    	console.log(time);
+    	var hours = parseInt(time[0]);
+    	var minutes = parseInt(time[1]);
+    	console.log(hours);
+    	console.log(minutes);
+    	minutes
+
+    };
+
+    convertTime("10:30");
+
+
+});
