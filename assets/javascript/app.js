5c40d3f177222b988a41bfa6663727f99b06a26c
Min Ryu <myyms@Mins-MacBook-Pro.local>
2017-11-08 21:07:48 -0800

    homework complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,7 +36,14 @@ $(document).ready(function() {
 
     database.ref("/train").on("child_added",function(snapshot){
 
-    	var newrow = $("<tr>");
+    	var frequency = snapshot.val().frequency;
+        var firstTime = moment(snapshot.val().time,"hh:mm").subtract(1,"years");
+        var diffTime = moment().diff(firstTime, "minutes")
+        var remainder = diffTime%frequency;
+        var tillTrain = frequency - remainder;
+        var nextTrain = moment().add(tillTrain,"minutes").format("hh:mm");
+
+        var newrow = $("<tr>");
     	var tr1 = $("<td>")
     	var tr2 = $("<td>")
     	var tr3 = $("<td>")
@@ -46,8 +53,8 @@ $(document).ready(function() {
     	tr1.append(snapshot.val().train_name);
     	tr2.append(snapshot.val().destination);
     	tr3.append(snapshot.val().frequency);
-    	tr4.append("blank");
-    	tr5.append("blank");
+    	tr4.append(nextTrain);
+    	tr5.append(tillTrain);
 
     	newrow.append(tr1, tr2, tr3, tr4, tr5);
 
